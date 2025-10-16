c006242affbd70625f22e0a6a8115b8f9a130314
Tran Quang Dang <tranquangdang21@gmail.com>
2025-10-16 16:57:00 +0700

    chore: reorganize test structure and update configurations

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: "./src/tests/e2e",
+  testDir: "./tests/e2e",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 1,
