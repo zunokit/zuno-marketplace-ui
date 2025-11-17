5a08ebedb314ea875a0fdfd9cc25457291d367b4
Trần Quang Đãng <tranquangdang21@gmail.com>
2025-11-17 21:41:19 +0700

    refactor: remove unused components from Product Discovery module
    - Deleted the `MarketplaceStats`, `NFTCategories`, and `TopCreators` components to streamline the Product Discovery module.
    - Updated the `ProductDiscovery` component to reflect these removals, enhancing code maintainability and reducing unnecessary complexity.
    
    These changes aim to simplify the codebase and improve the overall structure of the Product Discovery feature.

--- src/modules/product-discovery/index.tsx
+++ src/modules/product-discovery/index.tsx
@@ -3,9 +3,6 @@
 import { HomeBanner } from "@/modules/product-discovery/banner";
 import CarouselCollection from "@/modules/product-discovery/collection-carousel";
 import TrendingNFTs from "@/modules/product-discovery/trending-nfts";
-import TopCreators from "@/modules/product-discovery/top-creators";
-import MarketplaceStats from "@/modules/product-discovery/marketplace-stats";
-import NFTCategories from "@/modules/product-discovery/nft-categories";
 import LiveAuctions from "@/modules/product-discovery/live-auctions";
 
 export default function ProductDiscovery() {
@@ -15,9 +12,6 @@ export default function ProductDiscovery() {
       <LiveAuctions />
       <TrendingNFTs />
       <CarouselCollection />
-      <MarketplaceStats />
-      {/* <TopCreators /> */}
-      <NFTCategories />
     </div>
   );
 }
