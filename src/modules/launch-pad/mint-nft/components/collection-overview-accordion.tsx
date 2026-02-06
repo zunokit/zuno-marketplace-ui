4396d91eb12cda554b647daa06eb1c320462229d
Trần Quang Đãng <tranquangdang21@gmail.com>
2026-02-06 15:08:35 +0700

    refactor: update accordion components for improved styling and layout
    - Adjusted padding in CollectionOverviewAccordion and CollectionUtilityAccordion components for better spacing.
    - Modified hover styles on AccordionTrigger to enhance user interaction experience.
    - Removed defaultValue prop from Accordion to streamline component usage.

--- src/modules/launch-pad/mint-nft/components/collection-overview-accordion.tsx
+++ src/modules/launch-pad/mint-nft/components/collection-overview-accordion.tsx
@@ -14,10 +14,10 @@ export function CollectionOverviewAccordion({
   className,
 }: CollectionOverviewProps) {
   return (
-    <div className={cn("p-4 space-y-4", className)}>
-      <Accordion type="single" collapsible defaultValue="overview" className="w-full">
+    <div className={cn("px-6 py-4 space-y-4", className)}>
+      <Accordion type="single" collapsible className="w-full">
         <AccordionItem value="overview" className="!border-primary !bg-transparent border-0">
-          <AccordionTrigger className="hover:bg-transparent hover:no-underline py-4 pl-5 pr-4 [&>svg]:text-foreground">
+          <AccordionTrigger className="hover:underline hover:no-underline [&>svg]:text-foreground">
             <h2 className="text-xl font-bold text-wrap">Overview</h2>
           </AccordionTrigger>
           <AccordionContent>
